a45a0be8cc412b4e5ca09ec6cf524921ccfad727
Carl James <carl@jovian34.com>
2018-11-28 08:17:05 -0500

    condensed code - all tests still pass

--- week5/earliest.py
+++ week5/earliest.py
@@ -4,7 +4,6 @@ def get_earliest(*args):
     dates = []
     for date in args:
         month, day, year = date.split('/')
-        month, day, year = int(month), int(day), int(year)
-        dates.append((date, month, day, year))
+        dates.append((date, int(month), int(day), int(year)))
     dates.sort(key=operator.itemgetter(3, 1, 2))
     return dates[0][0]
