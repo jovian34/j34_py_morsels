9198be4fc27dd3e1c4bec9db0a61aa242a0ac68a
Carl James <carl@jovian34.com>
2019-07-20 19:39:46 -0400

    float_range now class with non-bonus tests passing

--- ex017/float_range.py
+++ ex017/float_range.py
@@ -1,12 +1,23 @@
-def float_range(start, stop='tester', step=1):
-    if stop == 'tester':
-        start, stop = 0, start
-    if step < 0:
-        while start > stop:
-            yield start
-            start = step + start
-    else:
-        while start < stop:
-            yield start
-            start += step
+
+class float_range:
+
+    def __init__(self, start, stop='tester', step=1.0):
+        self.start = start
+        self.stop = stop
+        self.step = step
+
+    def __iter__(self):
+        if self.stop == 'tester':
+            current_value, local_stop = 0, self.start
+        else:
+            current_value, local_stop = self.start, self.stop
+
+        if self.step < 0:
+            while current_value > local_stop:
+                yield current_value
+                current_value += self.step
+        else:
+            while current_value < local_stop:
+                yield current_value
+                current_value += self.step
 
