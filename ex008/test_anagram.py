0c0a60b2f39e4f644233fa2324f2c96ae9889e5b
Carl James <carl@jovian34.com>
2019-07-28 12:18:54 -0400

    oe008 passed with all bonuses

--- ex008/test_anagram.py
+++ ex008/test_anagram.py
@@ -26,7 +26,7 @@ class IsAnagramTests(unittest.TestCase):
         self.assertTrue(is_anagram("Listen", "silent"))
 
     # To test the Bonus part of this exercise, comment out the following line
-    @unittest.expectedFailure
+    # @unittest.expectedFailure
     def test_spaces_ignored(self):
         phrase1 = "William Shakespeare"
         phrase2 = "I am a weakish speller"
@@ -34,14 +34,14 @@ class IsAnagramTests(unittest.TestCase):
         self.assertFalse(is_anagram("a b c", "a b d"))
 
     # To test the Bonus part of this exercise, comment out the following line
-    @unittest.expectedFailure
+    # @unittest.expectedFailure
     def test_punctation_ignored(self):
         phrase1 = "A diet"
         phrase2 = "I'd eat"
         self.assertTrue(is_anagram(phrase1, phrase2))
 
     # To test the Bonus part of this exercise, comment out the following line
-    @unittest.expectedFailure
+    # @unittest.expectedFailure
     def test_characters_with_accents(self):
         self.assertTrue(is_anagram("Siobhán Donaghy", "Shanghai Nobody"))
 
