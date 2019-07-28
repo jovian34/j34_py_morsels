7f2498ba4690c4520f8284449d2a8040018c8d6b
Carl James <carl@jovian34.com>
2019-07-28 14:03:18 -0400

    ex009 1 test fails, all bonuses pass after remove print statements

--- ex009/multimax.py
+++ ex009/multimax.py
@@ -11,27 +11,19 @@ def multimax(iter_input, key=None):
 
     counts = Counter(iterable)
 
-    print(f"Inital counts are {counts}")
     try:
         maximum = max(counts)
-        print(f"The maximum is {maximum}")
     except ValueError:
-        print("This should only happen if empty")
         return output
     multi = counts[maximum]
-    print(f"The maximum happens {multi} times.")
     for i in range(multi):
         output.append(maximum)
-    print(f"The final output is {output}")
 
     if key:
         output = [item for item in iter_input if key(item) == maximum]
 
     return output
 
-if __name__ == "__main__":
-    multimax([1, 4, 2, 4, 3])
-    multimax([])
-    multimax([[0], [1], [], [0, 1], [1]])
+
 
 
