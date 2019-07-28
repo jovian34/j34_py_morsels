11f148f160f9e6134785c1f4bbf74c34b792713c
Carl James <carl@jovian34.com>
2019-07-28 14:24:01 -0400

    ex009 all tests pass including bonuses

--- ex009/multimax.py
+++ ex009/multimax.py
@@ -1,7 +1,7 @@
 from collections import Counter
 
 
-def multimax(iter_input, key=None):
+def multimax_old(iter_input, key=None):
     output = []
 
     if key:
@@ -24,6 +24,26 @@ def multimax(iter_input, key=None):
 
     return output
 
+def multimax(iter_input, key=None):
+    working_list = [item for item in iter_input]
+    try:
+        maximum = max(working_list)
+    except ValueError:
+        working_list = []
 
+    if key:
+        working_list = [key(item) for item in iter_input]
+        maximum = max(working_list)
+        return [
+            item
+            for item in iter_input
+            if key(item) == maximum
+        ]
+
+    return [
+        item
+        for item in working_list
+        if item == maximum
+    ]
 
 
