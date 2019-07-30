2e75475e8053017ee68b8081c97a8e1d49952021
Carl James <carl@jovian34.com>
2019-07-30 16:27:03 -0400

    parse_ranges pass with single item bonus

--- ex014/parse_ranges.py
+++ ex014/parse_ranges.py
@@ -2,9 +2,12 @@
 def parse_ranges(range_string: str):
     ranges = range_string.split(',')
     for a_range in ranges:
-        start, finish = a_range.split('-')
-        start = int(start)
-        finish = int(finish)
-        output_range = range(start, finish + 1)
-        for value in output_range:
-            yield value
+        if "-" in a_range:
+            start, finish = a_range.split('-')
+            start = int(start)
+            finish = int(finish)
+            output_range = range(start, finish + 1)
+            for value in output_range:
+                yield value
+        else:
+            yield int(a_range)
