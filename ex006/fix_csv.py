5be0e064908f75f6b1f2fd9ae56ef6485bda782e
Carl James <carl@jovian34.com>
2019-08-03 12:07:56 -0400

    ex006 all test and bonuses pass

--- ex006/fix_csv.py
+++ ex006/fix_csv.py
@@ -5,17 +5,27 @@ import csv
 parser = argparse.ArgumentParser()
 parser.add_argument('pipe_filename')
 parser.add_argument('csv_filename')
-parser.add_argument('--in-delimiter', help="the input delimiter", type=str, default='|')
+parser.add_argument('--in-delimiter', help="the input delimiter", type=str, default=None)
 parser.add_argument('--in-quote', help="the input quote character", type=str, default='"')
 args = parser.parse_args()
 
 lines = []
 
-with open(args.pipe_filename, 'r') as fr:
-    csv_reader = csv.reader(fr, delimiter=args.in_delimiter,
-                            quotechar=args.in_quote)
-    for line in csv_reader:
-        lines.append(line)
+if not args.in_delimiter:
+    with open(args.pipe_filename, 'r') as fr:
+        dialect = csv.Sniffer().sniff(fr.read(1024))
+        fr.seek(0)
+        csv_reader = csv.reader(fr, dialect)
+        for line in csv_reader:
+            lines.append(line)
+
+else:
+    args.in_delimiter == '|'
+    with open(args.pipe_filename, 'r') as fr:
+        csv_reader = csv.reader(fr, delimiter=args.in_delimiter,
+                                quotechar=args.in_quote)
+        for line in csv_reader:
+            lines.append(line)
 
 with open(args.csv_filename, 'w', newline='') as fw:
     csv_writer = csv.writer(fw, delimiter=',', quoting=csv.QUOTE_MINIMAL)
---
+++ ex006/fix_csv_old.py
@@ -0,0 +1,26 @@
+import argparse
+import csv
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('pipe_filename')
+parser.add_argument('csv_filename')
+parser.add_argument('--in-delimiter', help="the input delimiter", type=str, default='|')
+parser.add_argument('--in-quote', help="the input quote character", type=str, default='"')
+args = parser.parse_args()
+
+lines = []
+
+with open(args.pipe_filename, 'r') as fr:
+    csv_reader = csv.reader(fr, delimiter=args.in_delimiter,
+                            quotechar=args.in_quote)
+    for line in csv_reader:
+        lines.append(line)
+
+with open(args.csv_filename, 'w', newline='') as fw:
+    csv_writer = csv.writer(fw, delimiter=',', quoting=csv.QUOTE_MINIMAL)
+    for line in lines:
+        csv_writer.writerow(line)
+
+
+
