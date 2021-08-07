d38fe6389f28f3158e737485ccff0145ff240d62
Carl James <carl@jovian34.com>
2021-08-07 12:35:28 -0400

    initial attempt on ex027 - only one test passed

---
+++ ex027/interleave2.py
@@ -0,0 +1,13 @@
+
+def interleave(first_iter, second_iter):
+    first_dict = {}
+    second_dict = {}
+    for i, first_item in enumerate(first_iter):
+        first_dict[i] = first_item
+    for i, second_item in enumerate(second_iter):
+        second_dict[i] = second_item
+    for key, value in first_dict:
+        yield value
+        yield second_dict[key]
+
+
