936b2d024dce5ca7fdf0d2e479f59d67ff6a3411
Carl James <carl@jovian34.com>
2021-08-07 18:29:10 -0400

    ex027 - called items method on dict - all regular tests pass

--- ex027/interleave2.py
+++ ex027/interleave2.py
@@ -6,7 +6,7 @@ def interleave(first_iter, second_iter):
         first_dict[i] = first_item
     for i, second_item in enumerate(second_iter):
         second_dict[i] = second_item
-    for key, value in first_dict:
+    for key, value in first_dict.items():
         yield value
         yield second_dict[key]
 
