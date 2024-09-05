8a6b17005ae1383971455180d90fd547b1003606
Carl James <carl@jovian34.com>
2024-09-05 14:23:33 -0400

    ex021 two tests pass

--- ex021/suppress.py
+++ ex021/suppress.py
@@ -1,5 +1,11 @@
+from contextlib import contextmanager
 
+@contextmanager
+def suppress(error):
+    try:
+        suppress(error)
+    except error:
+        pass
 
-
-def suppress():
+    yield
     pass
